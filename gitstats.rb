cc52c90fdf8b6294f34425947aacebaccbeb3bc2
Tony <tonylc@gmail.com>
2013-04-25 00:40:16 -0700

    add by file type

--- gitstats.rb
+++ gitstats.rb
@@ -22,13 +22,12 @@ class Author
     @date_commits[date_str] = Commit.new
   end
 
-  def add_commits(date, added, deleted, line)
+  def add_commits(date, added, deleted, file_type)
     date_str = date.strftime("%Y%m%d")
     commit = @date_commits[date_str]
-    commit.added += added.to_i
-    commit.deleted += deleted.to_i
+    commit.add_file(file_type, added.to_i, deleted.to_i)
     if @name == "Tony"
-      debug("#{added}:#{deleted} - #{line}")
+      debug("#{added}:#{deleted} - #{file_type}")
     end
     @date_commits[date_str] = commit
   end
@@ -43,18 +42,46 @@ class CommitDate
 end
 
 class Commit
-  attr_accessor :added, :deleted
+  attr_accessor :rb_added, :rb_deleted,
+    :html_added, :html_deleted,
+    :css_added, :css_deleted,
+    :js_added, :js_deleted
+  #.rb|.scss|.coffee|.haml|.js|.css|.js.erb|.html.erb|.html
 
   def initialize
-    @added = 0
-    @deleted = 0
+    @rb_added = 0
+    @rb_deleted = 0
+    @html_added = 0
+    @html_deleted = 0
+    @css_added = 0
+    @css_deleted = 0
+    @js_added = 0
+    @js_deleted = 0
+  end
+
+  def add_file(file_type, lines_added, lines_deleted)
+    case file_type.to_sym
+      when :".rb"
+        @rb_added += lines_added
+        @rb_deleted += lines_deleted
+      when :".html", :".html.erb", :".haml"
+        @html_added += lines_added
+        @html_deleted += lines_deleted
+      when :".css", :".css.erb", :".scss"
+        @css_added += lines_added
+        @css_deleted += lines_deleted
+      when :".js", :".js.erb", :".coffee"
+        @js_added += lines_added
+        @js_deleted += lines_deleted
+      else
+        raise "Invalid file type #{file_type}"
+    end
   end
 
   def to_str
-    "#{added}:#{deleted}"
+    "rb - #{rb_added}:#{rb_deleted}\nhtml - #{html_added}:#{html_deleted}\njs - #{js_added}:#{js_deleted}\ncss - #{css_added}:#{css_deleted}\n"
   end
 end
-
 
 gitlog = `cat sample_git.txt`#`git log --numstat`
 
@@ -66,8 +93,7 @@ gitlog.split(/\n/).each do |line|
   if line.start_with?('Author:') # store author
     regex = line.match(/Author\:\s*([^<]*)<([^>]*)>/)
     if !regex
-      p "Author regex /Author\:\s*([^<]*)<([^>]*)>/ doesn't match #{line}"
-      next
+      raise "Author regex /Author\:\s*([^<]*)<([^>]*)>/ doesn't match #{line}"
     end
     author = Author.new(regex[1].strip, regex[2])
     # store author if they don't already exist
@@ -78,22 +104,22 @@ gitlog.split(/\n/).each do |line|
       author_hash[author.name] = author
     end
    next
-  elsif line.start_with?('Date:')
+  elsif line.start_with?('Date:') # store date of commit
     regex = line.match(/Date:\s+(.+)/)
     if !regex
-      p "Date regex /Date:\s+(.+)/ doesn't match #{line}"
+      raise "Date regex /Date:\s+(.+)/ doesn't match #{line}"
     end
     date = Date.strptime(regex[1], "%a %b %e")
     author.add_date(date)
     author_hash[author.name] = author
     next
   elsif line =~ /^\d+.+/ # store line count
-    regex = line.match(/(\d+)\s+(\d+)\s+(.+)$/)
+    regex = line.match(/(\d+)\s+(\d+)\s+.+(\.rb|\.scss|\.coffee|\.haml|\.js|\.css|\.css.erb|\.js.erb|\.html.erb|\.html)$/)
     if !regex
       p "Line count regex /(\d+)\s+(\d+)\s+(.+)$/ doesn't match #{line}"
       next
     end
-    author.add_commits(date, regex[1], regex[2], line)
+    author.add_commits(date, regex[1], regex[2], regex[3])
     author_hash[author.name] = author
     next
   end
